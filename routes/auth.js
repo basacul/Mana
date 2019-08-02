7aae842b327449db3f537bb5868ff12541ebaa52
basacul <antelo.lucas@outlook.com>
2019-08-02 21:07:18 +0000

    The user interfaces for password request and reset are implemented as well as the logic. Currently the password are not set to the new input, which will be handled in the next iteration.

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,7 @@ router.get("/", function (req, res) {
     if (req.isAuthenticated()) {
         res.redirect("home");
     } else {
-        res.render('login');
+        res.render('auth/login');
     }
 });
 
@@ -36,7 +36,7 @@ router.post("/login", middleware.isVerified, passport.authenticate('local', {
 
 router.get('/register', function (req, res) {
     if (!req.isAuthenticated()) {
-        res.render('register');
+        res.render('auth/register');
     } else {
         req.flash('error', 'Sign up only accessible if not logged in.');
         res.redirect('/home');
@@ -51,7 +51,7 @@ router.post('/register', function (req, res) {
         if (err) {
             console.log('Error on sign up', err);
             req.flash('error', err.message);
-            res.redirect('/register');
+            res.redirect('/auth/register');
         } else {
 			
             fs.mkdir(`${dir}/${req.body.username}`, { recursive: true }, (err) => {
@@ -92,22 +92,13 @@ router.post('/register', function (req, res) {
     });
 });
 
-// page to recover lost passwords
-router.get('/password', (req, res) => {
-		   
-});
-
-// request new password
-router.post('/password', (req, res) => {
-	
-});
 
 // allows user to verify with a secret token
 router.get('/verification', (req,res) => {
 	if (req.isAuthenticated()) {
         res.redirect("home");
     } else {
-        res.render('verification');
+        res.render('auth/verification');
     }
 });
 
@@ -140,6 +131,120 @@ router.post('/verification', (req, res) => {
 				}
 				
 			}
+			
+		}
+	});
+});
+
+// page to recover lost passwords
+router.get('/password', (req, res) => {
+	if (req.isAuthenticated()) {
+        res.redirect("home");
+    } else {
+        res.render('auth/password');
+    }
+});
+
+// request new password
+router.post('/password', (req, res) => {
+	User.findOne({token: req.body.token}, function(err, user){
+		if(!user){
+			
+			req.flash('error', 'Please make sure your token is correct.');
+			res.redirect('/password');
+			
+		}else{
+			
+			if(req.body.username === user.username && req.body.email === user.email){
+				// 1. generate secret token 
+				user.token = cryptoRandomString({length: 32, type: 'base64'});
+				// TODO: HOW TO RESET PASSWORD?
+				user.active = false;
+				user.save();
+
+				// 2. compose the email
+				const html = `Hi ${user.username}!
+				<br>
+				Send this email to ${user.email}.
+				<br>
+				Please ret your new password together with your new verification token. Please keep your token in 
+				a safe place as it will be used in the future. Your account is now set to inactive.
+				<br>
+				
+				Token: <strong>${user.token}</strong>
+				URL: <a href="https://mana-prototype.run.goorm.io/verification">https://mana-prototype.run.goorm.io/reset</a>
+				<br><br>
+				See you soon!`;
+
+				// 3. and send email. this function returns a promise
+
+				// email_for_dev should be replaced with user.email, but with MailGun I can only send mails to myself, yet
+				const email_for_dev = 'antelo.b.lucas@gmail.com';
+				mailer.sendEmail('registration@openhealth.care', email_for_dev, 'Reset your password', html);
+				
+				req.flash('success', 'You will shortly receive an email to set a new password');
+				res.redirect('/reset');
+				
+			}else{
+
+				if(user.username === req.body.username){
+					user.active = true;
+					user.save();
+					req.flash('error', `Please make sure your email is correct.`);
+					res.redirect('/password');
+				}else{
+					req.flash('error', 'Please make sure your username is correct.');
+					res.redirect('/password');
+				}
+				
+			}
+			
+		}
+	});
+});
+
+// get reset password page
+router.get('/reset', (req, res) => {
+	if (req.isAuthenticated()) {
+        res.redirect("home");
+    } else {
+        res.render('auth/reset');
+    }
+});
+
+// post credentials for getting a new password
+router.post('/reset', (req, res) => {
+	User.findOne({token: req.body.token}, function(err, user){
+		if(!user){
+			
+			req.flash('error', 'Please make sure your token is correct.');
+			res.redirect('/reset');
+			
+		}else{
+			
+			if(user.username === req.body.username){
+				if(req.body.password === req.body.passwordConfirmation){
+					
+					user.setPassword(req.body.password, err => {
+						if(err){
+							req.flash('error', 'New password could not be set by our application.');
+							return res.redirect('/');
+						}
+					});
+					user.active = true;
+					user.save();
+					req.flash('success', `Password reset was successful. You can now log in.`);
+					res.redirect('/');
+				}else{
+					req.flash('error', 'Please make sure your password and password confirmation are the same.');
+					res.redirect('/reset');
+				}
+				
+			}else{
+				req.flash('error', 'Please make sure your username is correct.');
+				res.redirect('/reset');
+			}
+			
 			
 		}
 	});
