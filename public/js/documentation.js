1627b467a40b691194a0f52292d5761b29c9e31f
basacul <antelo.lucas@outlook.com>
2019-08-12 15:10:21 +0000

    Application updated to reflect the development on hyperledger fabric.

--- public/js/documentation.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#description").addEventListener("click", function () {
-    if (this.innerHTML === "SHOW DESCRIPTION") {
-        this.innerHTML = "HIDE DESCRIPTION";
-        document.querySelector("#assignment").classList.toggle("hide");
-    } else {
-        this.innerHTML = "SHOW DESCRIPTION";
-        document.querySelector("#assignment").classList.toggle("hide");
-    }
-});
