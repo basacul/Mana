ee03b94b726e755e5aab4e39cce984f027b93d83
basacul <antelo.lucas@outlook.com>
2019-09-04 20:16:42 +0000

    Successfully implemented the creation of a new participant in hyperledger whenever a new user is verified. In addition, installed library axios as I had some difficulties with the implementation of https package for the post requests

--- routes/e-record.js
+++ routes/e-record.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router(); // now instead of app, use router
 const middleware = require('../middleware');
 const hyperledger = require('../utils/hyperledger');
-const https = require('https');
+
 
 router.get("/", middleware.isLoggedIn, (req, res) => {
 	const namespace = 'care.openhealth.mana.User';
 	let body;
+		
 	hyperledger.getAll(namespace).then(data => {
 		body = data;
 		console.log(body);
@@ -14,12 +15,20 @@ router.get("/", middleware.isLoggedIn, (req, res) => {
 		console.log(error);
 	}).finally(() => {
 		res.render("app/e-record", {body: body});
-	})
-	// const body = await response;
-	// console.log(body);
-	// res.render("app/e-record", {body: body});
+	});
+
 });
 
-
+// hyperledger.createUser(hyperledger.namespaces.user, mana._id.toString(), 'CLIENT' ).then(response => {
+// body = response.data;
+// console.log(body);
+// res.render("app/e-record", {body: body});
+// }).catch(error => {
+// console.log(error);
+// res.render("app/home");
+// });
+// // 	.finally(() => {
+// // 	res.render("app/e-record", {body: body});
+// // });
 
 module.exports = router;
--- utils/hyperledger.js
+++ utils/hyperledger.js
@@ -1,10 +1,76 @@
 const config = require('../config/hyperledger');
 const https = require('https');
-
-// in order to turn off rejections due to self signed certificates
+const axios = require('axios');
+/**
+* in order to turn off rejections due to self signed certificates
+* such as the hyperledger server 
+*/
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+
+const roles = {
+    CLIENT: 'CLIENT',
+    PROVIDER: 'PROVIDER',
+    INSURANCE: 'INSURANCE'
+};
+
+
+
 let hyperledger = {};
+
+hyperledger.namespaces = {
+	user: `${config.namespace}.User`,
+	item:  `${config.namespace}.Item`,
+	association:  `${config.namespace}.Association`,
+}
+
+hyperledger.createUser = function(namespace, manaId, role){
+	return axios.post(`${config.url}/${namespace}`, {
+		$class: hyperledger.namespaces.user,
+		manaId: manaId,
+		role: role
+	});
+};
+
+// hyperledger.createUser = function(namespace, manaId,role){
+// 	const postData = JSON.stringify({
+// 	  $class: hyperledger.namespaces.user,
+// 	  manaId: manaId,
+// 	  role: role
+// 	});
+	
+// 	const options = {
+// 		hostname: `${config.hostname}`,
+// 		port: config.port,
+// 		path: `api/${namespace}`,
+// 		method: 'POST',
+// 		headers: {
+// 			'Content-Type': 'application/json; charset=utf-8',
+// 			'Content-Length': Buffer.byteLength(postData),
+// 			'Accept': 'application/json',
+// 			'conncetion': 'Close'
+// 			}
+// 	};
+	
+	
+// 	return new Promise((resolve, reject) => {
+// 		const request = https.request(options, (response) => {
+// 			// if(response.statusCode < 200 || response.statusCode > 299){
+// 			// 	reject(new Error('Failed to load page, status code: ' + response.statusCode));
+// 			// }
+// 			// for the body I need to set the respective encoding 
+// 			response.setEncoding('utf8');
+// 			response.on('data', (body) => {
+// 				resolve(JSON.parse(body));
+// 			});
+// 		});
+		
+// 		request.on('error', (err) => reject(err));
+		
+// 		// write data to request body
+// 		request.write(postData);
+// 	});
+// }
 
 hyperledger.getAll = function(namespace){
 	return new Promise((resolve, reject) => {
@@ -16,28 +82,13 @@ hyperledger.getAll = function(namespace){
 			response.setEncoding('utf8');
 			response.on('data', (body) => {
 				resolve(JSON.parse(body));
-			})
+			});
 		});
-		
 		request.on('error', (err) => reject(err));
 	});
 	
 }
 
 
-// console.log("utils/hyperledger.js outside Promise");
-// 	return new Promise((resolve, reject) => {
-// 		console.log("utils/hyperledger.js inside Promise");
-// 		https.get(`${config.url}/${namespace}`, (response) => {
-// 			console.log("utils/hyperledger.js inside https.get in Promise");
-// 			console.log(response);
-// 			// for the body I need to set the respective encoding 
-// 			response.setEncoding('utf8')
-// 			response.on('body', (body) => {
-// 				console.log("utils/hyperledger.js inside response.on Promise");
-// 				console.log(body);
-// 				resolve(JSON.parse(body));
-// 			});
-// 		}).on('error', reject);
-// 	});
+
 module.exports = hyperledger;
